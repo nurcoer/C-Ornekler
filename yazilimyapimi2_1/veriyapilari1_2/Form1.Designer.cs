b1ec70e5da7286936723685041ddd19f2d73eae2
KadriyeNur <kadriyecoer@gmail.com>
2018-05-04 13:24:07 +0300

    Add files via upload

---
+++ yazilimyapimi2_1/veriyapilari1_2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace veriyapilari1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gbOyunAlani = new System.Windows.Forms.GroupBox();
+            this.btnOyunBasla = new System.Windows.Forms.Button();
+            this.cb9 = new System.Windows.Forms.CheckBox();
+            this.cb8 = new System.Windows.Forms.CheckBox();
+            this.cb7 = new System.Windows.Forms.CheckBox();
+            this.cb6 = new System.Windows.Forms.CheckBox();
+            this.cb5 = new System.Windows.Forms.CheckBox();
+            this.lblYeniOyun = new System.Windows.Forms.Label();
+            this.lblOyunKapa = new System.Windows.Forms.Label();
+            this.lblAdimSay = new System.Windows.Forms.Label();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.gbOyunAlani.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbOyunAlani
+            // 
+            this.gbOyunAlani.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.gbOyunAlani.Controls.Add(this.btnOyunBasla);
+            this.gbOyunAlani.Controls.Add(this.cb9);
+            this.gbOyunAlani.Controls.Add(this.cb8);
+            this.gbOyunAlani.Controls.Add(this.cb7);
+            this.gbOyunAlani.Controls.Add(this.cb6);
+            this.gbOyunAlani.Controls.Add(this.cb5);
+            this.gbOyunAlani.ForeColor = System.Drawing.Color.Coral;
+            resources.ApplyResources(this.gbOyunAlani, "gbOyunAlani");
+            this.gbOyunAlani.Name = "gbOyunAlani";
+            this.gbOyunAlani.TabStop = false;
+            // 
+            // btnOyunBasla
+            // 
+            this.btnOyunBasla.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.btnOyunBasla.ForeColor = System.Drawing.Color.Honeydew;
+            resources.ApplyResources(this.btnOyunBasla, "btnOyunBasla");
+            this.btnOyunBasla.Name = "btnOyunBasla";
+            this.btnOyunBasla.UseVisualStyleBackColor = true;
+            this.btnOyunBasla.Click += new System.EventHandler(this.btnOyunBasla_Click);
+            // 
+            // cb9
+            // 
+            resources.ApplyResources(this.cb9, "cb9");
+            this.cb9.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.cb9.ForeColor = System.Drawing.Color.Coral;
+            this.cb9.Name = "cb9";
+            this.cb9.UseVisualStyleBackColor = true;
+            // 
+            // cb8
+            // 
+            resources.ApplyResources(this.cb8, "cb8");
+            this.cb8.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.cb8.ForeColor = System.Drawing.Color.Coral;
+            this.cb8.Name = "cb8";
+            this.cb8.UseVisualStyleBackColor = true;
+            // 
+            // cb7
+            // 
+            resources.ApplyResources(this.cb7, "cb7");
+            this.cb7.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.cb7.ForeColor = System.Drawing.Color.Coral;
+            this.cb7.Name = "cb7";
+            this.cb7.UseVisualStyleBackColor = true;
+            // 
+            // cb6
+            // 
+            resources.ApplyResources(this.cb6, "cb6");
+            this.cb6.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.cb6.ForeColor = System.Drawing.Color.Coral;
+            this.cb6.Name = "cb6";
+            this.cb6.UseVisualStyleBackColor = true;
+            // 
+            // cb5
+            // 
+            resources.ApplyResources(this.cb5, "cb5");
+            this.cb5.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.cb5.ForeColor = System.Drawing.Color.Coral;
+            this.cb5.Name = "cb5";
+            this.cb5.UseVisualStyleBackColor = true;
+            // 
+            // lblYeniOyun
+            // 
+            resources.ApplyResources(this.lblYeniOyun, "lblYeniOyun");
+            this.lblYeniOyun.BackColor = System.Drawing.Color.Black;
+            this.lblYeniOyun.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblYeniOyun.Name = "lblYeniOyun";
+            this.lblYeniOyun.Click += new System.EventHandler(this.lblYeniOyun_Click_1);
+            // 
+            // lblOyunKapa
+            // 
+            resources.ApplyResources(this.lblOyunKapa, "lblOyunKapa");
+            this.lblOyunKapa.BackColor = System.Drawing.Color.Black;
+            this.lblOyunKapa.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblOyunKapa.Name = "lblOyunKapa";
+            this.lblOyunKapa.Click += new System.EventHandler(this.lblOyunKapa_Click_1);
+            // 
+            // lblAdimSay
+            // 
+            resources.ApplyResources(this.lblAdimSay, "lblAdimSay");
+            this.lblAdimSay.BackColor = System.Drawing.Color.Black;
+            this.lblAdimSay.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblAdimSay.Name = "lblAdimSay";
+            // 
+            // lblsonuc
+            // 
+            resources.ApplyResources(this.lblsonuc, "lblsonuc");
+            this.lblsonuc.BackColor = System.Drawing.Color.DarkRed;
+            this.lblsonuc.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblsonuc.Image = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.lblsonuc.Name = "lblsonuc";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::veriyapilari1_2.Properties.Resources.wallhaven_468618;
+            this.Controls.Add(this.lblYeniOyun);
+            this.Controls.Add(this.lblAdimSay);
+            this.Controls.Add(this.lblOyunKapa);
+            this.Controls.Add(this.gbOyunAlani);
+            this.Controls.Add(this.lblsonuc);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.gbOyunAlani.ResumeLayout(false);
+            this.gbOyunAlani.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbOyunAlani;
+        private System.Windows.Forms.CheckBox cb9;
+        private System.Windows.Forms.CheckBox cb8;
+        private System.Windows.Forms.CheckBox cb7;
+        private System.Windows.Forms.CheckBox cb6;
+        private System.Windows.Forms.CheckBox cb5;
+        private System.Windows.Forms.Button btnOyunBasla;
+        private System.Windows.Forms.Label lblYeniOyun;
+        private System.Windows.Forms.Label lblOyunKapa;
+        private System.Windows.Forms.Label lblAdimSay;
+        private System.Windows.Forms.Label lblsonuc;
+    }
+}
+
---
+++ yazilimyapimi2_1/veriyapilari1_2/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veriyapilari1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac = 0, OyunBitis = 0;
+        int X, Y;
+        public void OyunAlani(int a)
+        {
+            Button[] btn = new Button[a];
+            OyunBitis = a;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    btn[i] = new Button();
+                    btn[i].Name = (i + 1).ToString() + (j + 1).ToString();
+                    btn[i].Text = 0.ToString();
+                    btn[i].BackColor = Color.White;
+                    btn[i].Size = new Size(30, 30);
+                    btn[i].Location = new Point(i * 30, j * 30);
+                    this.Controls.Add(btn[i]);
+                    btn[i].Click += new EventHandler(btn_Click);
+                }
+            }
+            temizle();
+        }
+        public void temizle()
+        {
+            for (int i = 0; i < OyunBitis; i++)
+            {
+                for (int j = 0; j < OyunBitis; j++)
+                {
+                    if (((Button)this.Controls[(i+1).ToString() + (j+1).ToString()]).Enabled!=false)
+                    {
+                        ((Button)this.Controls[(i+1).ToString() + (j+1 ).ToString()]).Text = 0.ToString();
+                        ((Button)this.Controls[(i+1 ).ToString() + (j+1).ToString()]).BackColor = Color.White;
+                    }
+                }
+            }        
+        }
+        public void btn_Click(object sender, EventArgs e)
+        {
+            if (sayac==0)
+            {
+                ((Button)sender).Text = 1.ToString();
+                X = int.Parse(((Button)sender).Name.Remove(1, 1));
+                Y = int.Parse(((Button)sender).Name.Remove(0, 1));
+            }
+            if (((Button)sender).Text==1.ToString())
+            {
+                sayac++;
+                ((Button)sender).Text = 1.ToString() ;
+                ((Button)sender).Enabled = false;
+                temizle();
+                X = int.Parse(((Button)sender).Name.Substring(0, 1));
+                Y = int.Parse(((Button)sender).Name.Substring(1, 1));
+                int xYeni = 2, yYeni = 1, OyunDevam = 0 ;
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int j = 0; j <= 3; j++)
+                    {
+                        if ((X - xYeni) > 0 && (Y - yYeni) > 0 && (X - xYeni) <= OyunBitis && (Y - yYeni) <= OyunBitis && ((Button)this.Controls[(X - xYeni).ToString() + (Y - yYeni).ToString()]).Text != 1.ToString())
+                        {
+                            ((Button)this.Controls[(X - xYeni).ToString() + (Y - yYeni).ToString()]).Text = 1.ToString();
+                            ((Button)this.Controls[(X - xYeni).ToString() + (Y - yYeni).ToString()]).BackColor = Color.Blue;
+                            OyunDevam++;
+                        }
+                        switch (j)
+                        {
+                            case 0:
+                                yYeni *= -1;
+                                break;
+                            case 1:
+                                xYeni *= -1;
+                                break;
+                            case 2:
+                                yYeni *= -1;
+                                break;
+                            default:
+                                xYeni *= -1;
+                                break;
+                        }
+                    }
+                    switch (k)
+                    {
+                        case 0:
+                            yYeni = 2; xYeni = 1;
+                            break;
+                    }
+                }
+            ((Button)sender).BackColor = Color.Black;
+                if (OyunDevam == 0)
+                    lblsonuc.Text = "Game Over!.. :(";
+                lblAdimSay.Visible = true;
+                lblAdimSay.Text ="Tıklama sayısı="+sayac.ToString();
+            }
+            else MessageBox.Show("lütfen renkli noktaları seçiniz..");
+            if (sayac == (OyunBitis*OyunBitis))
+                lblsonuc.Text = "Kazandınız. :D";
+        }
+        private void lblOyunKapa_Click_1(object sender, EventArgs e)
+        {
+                DialogResult GirisKapanis = MessageBox.Show("programı kapatmak istediğinizden eminmisiniz?", "ÇIKIŞ", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (GirisKapanis == DialogResult.No)
+                {
+                    return;
+                }
+                Environment.Exit(0);
+        }
+        private void lblYeniOyun_Click_1(object sender, EventArgs e)
+        {
+            Ctrl_Ekle();
+            gbOyunAlani.Visible = true;
+            lblAdimSay.Visible = false;
+            lblOyunKapa.Visible = false;
+            lblYeniOyun.Visible = false;
+            sayac = 0;
+        }
+        public void Ctrl_Ekle()
+        { 
+            this.Controls.Clear();
+            this.Controls.Add(gbOyunAlani);
+            this.Controls.Add(lblAdimSay);
+            this.Controls.Add(lblOyunKapa);
+            this.Controls.Add(lblsonuc);
+            this.Controls.Add(lblYeniOyun);
+        }
+
+        private void btnOyunBasla_Click(object sender, EventArgs e)
+        {
+
+            gbOyunAlani.Visible = false;
+            lblOyunKapa.Visible = true;
+            lblYeniOyun.Visible = true;
+            if (cb5.Checked == true && cb6.Checked == false && cb7.Checked == false && cb8.Checked == false && cb9.Checked == false)
+                OyunAlani(5);
+            else if (cb6.Checked == true && cb5.Checked == false && cb7.Checked == false && cb8.Checked == false && cb9.Checked == false)
+                OyunAlani(6);
+            else if (cb7.Checked == true && cb6.Checked == false && cb5.Checked == false && cb8.Checked == false && cb9.Checked == false)
+                OyunAlani(7);
+            else if (cb8.Checked == true && cb6.Checked == false && cb7.Checked == false && cb5.Checked == false && cb9.Checked == false)
+                OyunAlani(8);
+            else if (cb9.Checked == true && cb6.Checked == false && cb7.Checked == false && cb8.Checked == false && cb5.Checked == false)
+                OyunAlani(9);
+            else MessageBox.Show("lütfen oyun alanınızı seçiniz.");
+        }
+    }
+}
